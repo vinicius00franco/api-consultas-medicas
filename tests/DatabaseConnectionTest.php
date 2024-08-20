642ba0f1eb77ee001a5cf24ec00b0164e4b1c500
vinicius00franco <vincus.franco.silva@gmail.com>
2024-08-20 12:13:02 -0300

    BUSINESS LOGIC - make other controllers, one test reamins failed and one rissk

--- tests/DatabaseConnectionTest.php
+++ tests/DatabaseConnectionTest.php
@@ -14,6 +14,12 @@ class DatabaseConnectionTest extends KernelTestCase
         self::bootKernel();
         $container = self::getContainer();
         $this->entityManager = $container->get(EntityManagerInterface::class);
+
+        if (!$this->entityManager) {
+            throw new \Exception('Entity Manager could not be instantiated.');
+        }
+
+        var_dump(getenv('DATABASE_URL'));
     }
 
     public function testEntityManagerConnection(): void
@@ -21,15 +27,28 @@ class DatabaseConnectionTest extends KernelTestCase
         $this->assertInstanceOf(EntityManagerInterface::class, $this->entityManager, 'Entity Manager should be an instance of EntityManagerInterface.');
 
         $connection = $this->entityManager->getConnection();
-        $this->assertTrue($connection->isConnected(), 'Entity Manager connection should be established.');
+        $this->assertNotNull($connection, 'Connection should not be null.');
 
-        $result = $connection->executeQuery('SELECT 1');
-        $this->assertEquals(1, $result->fetchOne(), 'Entity Manager query should return 1.');
+        try {
+
+            $this->assertTrue($connection->isConnected(), 'Entity Manager connection should be established.');
+        } catch (\Exception $e) {
+            $this->fail('Failed to establish connection: ' . $e->getMessage());
+        }
+
+        try {
+            $result = $connection->executeQuery('SELECT 1');
+            $this->assertEquals(1, $result->fetchOne(), 'Entity Manager query should return 1.');
+        } catch (\Exception $e) {
+            $this->fail('Query execution failed: ' . $e->getMessage());
+        }
     }
 
     protected function tearDown(): void
     {
-        $this->entityManager->close();
+        if ($this->entityManager->isOpen()) {
+            $this->entityManager->close();
+        }
 
         parent::tearDown();
     }
