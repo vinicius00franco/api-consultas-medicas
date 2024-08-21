0e31aa38cc9d6d0228eb1ba6801fc8c2c7d6b519
vinicius00franco <vincus.franco.silva@gmail.com>
2024-08-21 10:44:44 -0300

    REFACTORY - fix to use attribute type

--- src/Validator/Constraints/HospitalExists.php
+++ src/Validator/Constraints/HospitalExists.php
@@ -6,10 +6,20 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use App\Repository\HospitalRepository;
 
-/**
- * @Annotation
- */
+
+#[\Attribute]
 class HospitalExists extends Constraint
 {
     public $message = 'O hospital selecionado não existe.';
+
+    public function __construct(string $message = null, array $groups = null, mixed $payload = null)
+    {
+        parent::__construct($groups, $payload);
+        $this->message = $message ?? $this->message;
+    }
+
+    public function validatedBy(): string
+    {
+        return \App\Validator\HospitalExistsValidator::class;
+    }
 }
